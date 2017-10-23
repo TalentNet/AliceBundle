a4d6e5f37ea1beb8d40a9ed7ee0778bda6527777
Maurice Bennett <mbennett@talentnet.com>
2017-10-23 00:17:02 -0400

    Added support for doctrine ODM

---
+++ src/Console/Command/Doctrine/DoctrineOdmLoadDataFixturesCommand.php
@@ -0,0 +1,130 @@
+<?php
+namespace Hautelook\AliceBundle\Console\Command\Doctrine;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Hautelook\AliceBundle\LoaderInterface as AliceBundleLoaderInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application as FrameworkBundleConsoleApplication;
+use Symfony\Component\Console\Application as ConsoleApplication;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+/**
+ * Command used to load the fixtures.
+ *
+ * @author Théo FIDRY <theo.fidry@gmail.com>
+ */
+final class DoctrineOdmLoadDataFixturesCommand extends Command
+{
+	/**
+	 * @var ManagerRegistry
+	 */
+	private $doctrine;
+	/**
+	 * @var AliceBundleLoaderInterface
+	 */
+	private $loader;
+	public function __construct(string $name, ManagerRegistry $managerRegistry, AliceBundleLoaderInterface $loader)
+	{
+		parent::__construct($name);
+		$this->doctrine = $managerRegistry;
+		$this->loader = $loader;
+	}
+	/**
+	 * @inheritdoc
+	 */
+	protected function configure()
+	{
+		$this
+			->setAliases(['hautelook:odm:fixtures:load'])
+			->setDescription('Load ODM data fixtures to your database.')
+			->addOption(
+				'bundle',
+				'b',
+				InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
+				'Bundles where fixtures should be loaded.'
+			)
+			->addOption(
+				'manager',
+				'dm',
+				InputOption::VALUE_REQUIRED,
+				'The document manager to use for this command. If not specified, use the default Doctrine fixtures document '
+				.'manager.'
+			)
+			->addOption(
+				'append',
+				null,
+				InputOption::VALUE_NONE,
+				'Append the data fixtures instead of deleting all data from the database first.'
+			)
+			->addOption('purge',
+				null,
+				InputOption::VALUE_NONE,
+				'Purge data before loading fixtures.'
+			)
+		;
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function setApplication(ConsoleApplication $application = null)
+	{
+		if (null !== $application && false === $application instanceof FrameworkBundleConsoleApplication) {
+			throw new \InvalidArgumentException(
+				sprintf(
+					'Expected application to be an instance of "%s".',
+					FrameworkBundleConsoleApplication::class
+				)
+			);
+		}
+		parent::setApplication($application);
+	}
+	/**
+	 * {@inheritdoc}
+	 *
+	 * \RuntimeException Unsupported Application type
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		// Warn the user that the database will be purged
+		// Ask him to confirm his choice
+		if ($input->isInteractive() && !$input->getOption('append')) {
+			if (false === $this->askConfirmation(
+					$input,
+					$output,
+					'<question>Careful, database will be purged. Do you want to continue y/N ?</question>',
+					false
+				)
+			) {
+				return 0;
+			}
+		}
+		$manager = $this->doctrine->getManager($input->getOption('manager'));
+		$environment = $input->getOption('env');
+		$bundles = $input->getOption('bundle');
+		$append = $input->getOption('append');
+		$truncate = $input->getOption('purge');
+		/** @var FrameworkBundleConsoleApplication $application */
+		$application = $this->getApplication();
+		$this->loader->load($application, $manager, $bundles, $environment, $append, $truncate);
+		return 0;
+	}
+	/**
+	 * Prompts to the user a message to ask him a confirmation.
+	 *
+	 * @param InputInterface  $input
+	 * @param OutputInterface $output
+	 * @param string          $question
+	 * @param bool            $default
+	 *
+	 * @return bool User choice
+	 */
+	private function askConfirmation(InputInterface $input, OutputInterface $output, $question, $default)
+	{
+		/** @var QuestionHelper $questionHelper */
+		$questionHelper = $this->getHelperSet()->get('question');
+		$question = new ConfirmationQuestion($question, $default);
+		return (bool) $questionHelper->ask($input, $output, $question);
+	}
+}
---
+++ src/Loader/DoctrineOdmLoader.php
@@ -0,0 +1,124 @@
+<?php
+namespace Hautelook\AliceBundle\Loader;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Fidry\AliceDataFixtures\Bridge\Doctrine\Persister\ObjectManagerPersister;
+use Fidry\AliceDataFixtures\Bridge\Doctrine\Purger\Purger;
+use Fidry\AliceDataFixtures\Loader\FileResolverLoader;
+use Fidry\AliceDataFixtures\Loader\PurgerLoader;
+use Fidry\AliceDataFixtures\LoaderInterface;
+use Fidry\AliceDataFixtures\Persistence\PersisterAwareInterface;
+use Hautelook\AliceBundle\BundleResolverInterface;
+use Hautelook\AliceBundle\FixtureLocatorInterface;
+use Hautelook\AliceBundle\LoaderInterface as AliceBundleLoaderInterface;
+use Hautelook\AliceBundle\LoggerAwareInterface;
+use Hautelook\AliceBundle\Resolver\File\KernelFileResolver;
+use Nelmio\Alice\IsAServiceTrait;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\HttpKernel\KernelInterface;
+final class DoctrineOdmLoader implements AliceBundleLoaderInterface, LoggerAwareInterface
+{
+	use IsAServiceTrait;
+	/**
+	 * @var BundleResolverInterface
+	 */
+	private $bundleResolver;
+	/**
+	 * @var FixtureLocatorInterface
+	 */
+	private $fixtureLocator;
+	/**
+	 * @var LoaderInterface|PersisterAwareInterface
+	 */
+	private $loader;
+	/**
+	 * @var LoggerInterface
+	 */
+	private $logger;
+	public function __construct(
+		BundleResolverInterface $bundleResolver,
+		FixtureLocatorInterface $fixtureLocator,
+		LoaderInterface $loader,
+		LoggerInterface $logger
+	) {
+		$this->bundleResolver = $bundleResolver;
+		$this->fixtureLocator = $fixtureLocator;
+		if (false === $loader instanceof PersisterAwareInterface) {
+			throw new \InvalidArgumentException(
+				sprintf(
+					'Expected loader to be an instance of "%s".',
+					PersisterAwareInterface::class
+				)
+			);
+		}
+		$this->loader = $loader;
+		$this->logger = $logger;
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function withLogger(LoggerInterface $logger): self
+	{
+		return new self($this->bundleResolver, $this->fixtureLocator, $this->loader, $logger);
+	}
+	/**
+	 * @inheritdoc
+	 */
+	public function load(
+		Application $application,
+		DocumentManager $manager,
+		array $bundles,
+		string $environment,
+		bool $append,
+		bool $purge
+	) {
+		$bundles = $this->bundleResolver->resolveBundles($application, $bundles);
+		$fixtureFiles = $this->fixtureLocator->locateFiles($bundles, $environment);
+		$this->logger->info('fixtures found', ['files' => $fixtureFiles]);
+		$fixtures = $this->loadFixtures(
+			$this->loader,
+			$application->getKernel(),
+			$manager,
+			$fixtureFiles,
+			$application->getKernel()->getContainer()->getParameterBag()->all(),
+			$append,
+			$purge
+		);
+		$this->logger->info('fixtures loaded');
+		return $fixtures;
+	}
+	/**
+	 * @param LoaderInterface|PersisterAwareInterface $loader
+	 * @param KernelInterface                         $kernel
+	 * @param DocumentManager                         $manager
+	 * @param string[]                                $files
+	 * @param array                                   $parameters
+	 * @param bool                                    $append
+	 * @param bool|null                               $purge
+	 *
+	 * @return \object[]
+	 */
+	private function loadFixtures(
+		LoaderInterface $loader,
+		KernelInterface $kernel,
+		DocumentManager $manager,
+		array $files,
+		array $parameters,
+		bool $append,
+		bool $purge = false
+	) {
+		if ($append === true && $purge === true) {
+			throw new \LogicException(
+				'Cannot append loaded fixtures and at the same time purge the database. Choose one.'
+			);
+		}
+		$loader = $loader->withPersister(new ObjectManagerPersister($manager));
+		if (true === $append) {
+			return $loader->load($files, $parameters);
+		}
+		$purger = new Purger($manager);
+		$loader = new PurgerLoader($loader, $purger);
+		$loader = new FileResolverLoader($loader, new KernelFileResolver($kernel));
+		return $loader->load($files, $parameters);
+	}
+}
